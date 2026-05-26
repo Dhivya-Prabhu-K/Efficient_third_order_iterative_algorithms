c90edf006e3565de1a49df7612a085714a7cdc7a
Dhivya Prabhu K <dhivyanslm@gmail.com>
2026-05-26 08:53:39 +0530

    Update Error_comparison_legendre.m

--- Error_comparison/Legendre/Error_comparison_legendre.m
+++ Error_comparison/Legendre/Error_comparison_legendre.m
@@ -1,6 +1,6 @@
 clear all
 clc;
-n=input('value of n=');
+n=10000;
 
 % Storing values
 [x1, y1] = compare_leg_fourth_1(n);         
@@ -23,53 +23,3 @@ set(gca, 'FontSize', 12);
 
 
 
-%%
-s1=y1;
-s2=y2;
-% s3=time_method3;
-s3=y3;
-a=zeros(1, length(s1));
-for k=1:length(s1)
-    a(k) = min([s1(k), s2(k), s3(k)]);
-end
-m=1.1;
-for i=1:length(s1)
-    t1(i) = s1(i)/a(i);
-    t2(i) = s2(i)/a(i);
-    
-    t3(i) = s3(i)/a(i);
-end
-
-tow = 0.98:0.00005:m;
-
-n1 = zeros(1, length(tow));
-n2 = zeros(1, length(tow));
-
-n3 = zeros(1, length(tow));
-
-for i = 1:length(tow)
-    n1(i) = sum(t1 < tow(i));
-    n2(i) = sum(t2 < tow(i));
-   
-    n3(i) = sum(t3 < tow(i));
-end
-
-% Normalize
-n1 = n1 / length(s1);
-n2 = n2 / length(s1);
-
-n3 = n3 / length(s1);
-
-% Plot
-figure;
-plot(tow, n1, 'r', 'LineWidth', 2); hold on;
-plot(tow, n2, 'k', 'LineWidth', 2);
-% plot(tow, n3, 'g', 'LineWidth', 2);
-plot(tow, n3, 'b', 'LineWidth', 2);
-hold off;
-
-xlabel('\tau');
-ylabel('\rho_s(\tau)');
-legend({'FOM-L','SOM-L','TOM-L'}, 'Location','northwest');
-title('Comparison Plot');
-grid on;
